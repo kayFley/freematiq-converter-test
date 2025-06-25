2a0da41259fd0b33e700d814587613401bb4107b
kayFley <vasya.shumilov.2003@gmail.com>
2025-06-25 10:35:22 +0700

    fixes, mode toggle

--- src/Converter.tsx
+++ src/Converter.tsx
@@ -1,40 +1,45 @@
 import { PairItem } from '@/components/pair-item/pair-item'
 import { Button } from '@/components/ui/button/button'
+import styles from '@/converter.module.scss'
+import { ThemeProvider } from '@/hooks/theme-provider'
 import { ConversionPair, converterStore } from '@/stores/converter'
 import { PlusIcon } from 'lucide-react'
 import { observer } from 'mobx-react-lite'
 import { AnimatePresence, motion } from 'motion/react'
 import { useCallback } from 'react'
-import styles from './converter.module.scss'
+import { ModeToggle } from './components/mode-toggle/mode-toggle'
 
 export const Converter = observer(() => {
 	const handleAddPair = useCallback(() => converterStore.addPair(), [])
 
 	return (
-		<div className={`dark ${styles.container}`}>
-			<AnimatePresence>
-				{converterStore.pairs.map((pair: ConversionPair) => (
-					<motion.div
-						key={pair.id}
-						initial={{ opacity: 0, height: 0 }}
-						animate={{ opacity: 1, height: 'auto' }}
-						exit={{ opacity: 0, height: 0 }}
-						transition={{ duration: 0.2 }}
-						layout
-					>
-						<PairItem key={pair.id} pair={pair} />
-					</motion.div>
-				))}
-			</AnimatePresence>
+		<ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+			<ModeToggle />
+			<div className={`${styles.container}`}>
+				<AnimatePresence>
+					{converterStore.pairs.map((pair: ConversionPair) => (
+						<motion.div
+							key={pair.id}
+							initial={{ opacity: 0, height: 0 }}
+							animate={{ opacity: 1, height: 'auto' }}
+							exit={{ opacity: 0, height: 0 }}
+							transition={{ duration: 0.2 }}
+							layout
+						>
+							<PairItem key={pair.id} pair={pair} />
+						</motion.div>
+					))}
+				</AnimatePresence>
 
-			<Button
-				size='lg'
-				className={styles.addButton}
-				onClick={handleAddPair}
-			>
-				<PlusIcon />
-				Добавить пару валют
-			</Button>
-		</div>
+				<Button
+					size='lg'
+					className={styles.addButton}
+					onClick={handleAddPair}
+				>
+					<PlusIcon />
+					Добавить пару валют
+				</Button>
+			</div>
+		</ThemeProvider>
 	)
 })
